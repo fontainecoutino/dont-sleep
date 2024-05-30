897bd87cf1cccdb6edd8fc79a4c30d784b861164
Fontaine Coutino <65735721+fontainecoutino@users.noreply.github.com>
2024-05-29 22:07:40 -0700

    feat: exit page

--- app/exit.go
+++ app/exit.go
@@ -2,17 +2,23 @@ package app
 
 import (
 	"fmt"
+
+	"github.com/charmbracelet/lipgloss"
 )
 
 // ── view ────────────────────────────────────────────────────────────
+
+var (
+	exitMainTitleStyle   = lipgloss.NewStyle().Bold(true)
+	exitSecondTitleStyle = lipgloss.NewStyle().Italic(true)
+)
 
 func errorView(m Model) string {
 	return fmt.Sprintf("\nThere was an error:\n%s\n\n", m.Err.Error())
 }
 
 func quitView(m Model) string {
-	// TODO: add time elapsed
-	tmp := "\n  I am finally allowed to sleep!\n\n"
-
+	tmp := exitMainTitleStyle.Render("\nI am finally allowed to sleep!") + "\n"
+	tmp += exitSecondTitleStyle.Render(fmt.Sprintf(" (awake for %d sec)\n\n", m.TimeAwake))
 	return tmp
 }
